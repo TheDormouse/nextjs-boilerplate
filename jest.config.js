4e32f1191cb0a6a9a9087b3fe1a45d76b0c0635c
Dormouse <madmarchy@live.com>
2020-11-26 21:51:50 -0500

    adding pages for api routes later

--- jest.config.js
+++ jest.config.js
@@ -121,7 +121,8 @@ module.exports = {
 
   // A list of paths to directories that Jest should use to search for files in
    roots: [
-     "<rootDir>/components"
+     "<rootDir>/components",
+     "<rootDir>/pages"
    ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
